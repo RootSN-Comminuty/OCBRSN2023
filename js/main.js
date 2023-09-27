f0653684d6cb919976d081f373e14868343c26f1
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-09-27 02:59:29 +0000

    UPDATE COUNTER OCTOBER MOUNTH

--- js/main.js
+++ js/main.js
@@ -96,27 +96,43 @@
         CountDown
     --------------------*/
     // For demo preview
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
+    // var today = new Date();
+    // var dd = String(today.getDate()).padStart(2, '0');
+    // var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    // var yyyy = today.getFullYear();
 
-    if(mm == 12) {
-        mm = '01';
-        yyyy = yyyy + 1;
-    } else {
-        mm = parseInt(mm) + 1;
-        mm = String(mm).padStart(2, '0');
-    }
-    var timerdate = mm + '/' + dd + '/' + yyyy;
+    // if(mm == 12) {
+    //     mm = '01';
+    //     yyyy = yyyy + 1;
+    // } else {
+    //     mm = parseInt(mm) + 1;
+    //     mm = String(mm).padStart(2, '0');
+    // }
+    // var timerdate = mm + '/' + dd + '/' + yyyy;
     // For demo preview end
     
 
     // Use this for real timer date
     /*  var timerdate = "2020/01/01"; */
 
+	// $("#countdown").countdown(timerdate, function(event) {
+    //     $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Jours</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Heures</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secondes</p> </div>"));
+    // });
+    var yyyy = 2023;
+    var mm = 11;
+    var dd = 1;
+
+    while (mm > 12) {
+      yyyy = yyyy + 1;
+      mm = parseInt(mm) - 12;
+      mm = String(mm).padStart(2, '0');
+    }
+
+    var timerdate = mm + '/' + dd + '/' + yyyy;
+
 	$("#countdown").countdown(timerdate, function(event) {
-        $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
+        $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Jours</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Heures</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secondes</p> </div>"));
     });
+
 
 })(jQuery);
