d20bb2764d2a05f0766e83dcdbe086d7355332b4
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-10-11 23:38:00 +0000

    Update

--- js/main.js
+++ js/main.js
@@ -89,8 +89,31 @@
         Magnific Popup
     --------------------*/
     $('.video-popup').magnificPopup({
-        type: 'iframe'
-    });
+    type: 'iframe',
+    iframe: {
+        patterns: {
+            youtube: {
+                index: 'youtube.com/', 
+                id: function(url) {        
+                    var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+                    if ( !m || !m[1] ) return null;
+                    return m[1];
+                },
+                src: '//www.youtube.com/embed/%id%?autoplay=1'
+            },
+            vimeo: {
+                index: 'vimeo.com/', 
+                id: function(url) {        
+                    var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+                    if ( !m || !m[5] ) return null;
+                    return m[5];
+                },
+                src: '//player.vimeo.com/video/%id%?autoplay=1'
+            }
+        }
+    }
+});
+
 
     /*------------------
         CountDown
@@ -136,3 +159,4 @@
 
 
 })(jQuery);
+
